825f8e9a712de5f7783664206ccc9233685bca68
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2021-12-07 21:17:44 +0900

    Add tap function

--- lib/screens/play.dart
+++ lib/screens/play.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class Play extends StatefulWidget {
   static const String id = "/play";
@@ -9,10 +10,47 @@ class Play extends StatefulWidget {
 }
 
 class _PlayState extends State<Play> {
+  int score = 0;
+  var _isPlaying = false;
+  late Timer _timer;
+
+  void handleTapDown(e) {
+    score = 0;
+    _timer = Timer.periodic(Duration(milliseconds: 10), (t) {
+      setState(() {
+        score++;
+      });
+    });
+  }
+
+  void handleTapUp(e) {
+    print("hadle up");
+    _timer.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Play'),
+    return SafeArea(
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Expanded(
+                flex: 1,
+                child: Container(
+                    alignment: Alignment.center,
+                    child: Text('Your score is $score'))),
+            Expanded(
+                flex: 1,
+                child: GestureDetector(
+                    onPanDown: handleTapDown,
+                    onPanEnd: handleTapUp,
+                    child: Container(
+                      color: Colors.grey,
+                    )))
+          ],
+        ),
+      ),
     );
   }
 }
