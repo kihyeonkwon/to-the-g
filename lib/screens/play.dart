64c335f27ab9529017f4caad0776f8dcb0d196c7
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2022-02-09 15:20:48 +0900

    Add read from firebase

--- lib/screens/play.dart
+++ lib/screens/play.dart
@@ -14,6 +14,63 @@ class _PlayState extends State<Play> {
   int score = 0;
   var _isPlaying = false;
   late Timer _timer;
+  final myTextController = TextEditingController();
+  DocumentReference documentReference =
+      FirebaseFirestore.instance.collection('scores').doc();
+
+  Stream<List> readScores() => FirebaseFirestore.instance
+      .collection('scores')
+      .snapshots()
+      .map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList());
+
+  @override
+  void dispose() {
+    myTextController.dispose();
+    super.dispose();
+  }
+
+  _uploadData() async {
+    documentReference.set({
+      'score': score,
+      'created_at': Timestamp.now(),
+      'message': myTextController.text,
+      'effect': 0,
+    });
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('디지셨습니다.'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('This is a demo alert dialog.'),
+                Text('Would you like to approve of this message?'),
+                TextField(controller: myTextController)
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('데이터 보내기!'),
+              onPressed: () {
+                _uploadData();
+                Navigator.of(context).pop();
+                setState(() {
+                  score = 0;
+                  myTextController.clear();
+                });
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   void handleTapDown(e) {
     score = 0;
@@ -25,18 +82,13 @@ class _PlayState extends State<Play> {
   }
 
   void handleTapUp(e) {
-    print("hadle up");
+    print("handle up");
+    _showMyDialog();
     _timer.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
-    _uploadData() async {
-      DocumentReference documentReference =
-          FirebaseFirestore.instance.collection('scores').doc();
-      documentReference.set({'score': 6767867});
-    }
-
     return SafeArea(
       child: Container(
         color: Colors.white,
@@ -58,14 +110,16 @@ class _PlayState extends State<Play> {
                     child: Container(
                       color: Colors.grey,
                     ))),
-            Expanded(
-              flex: 1,
-              child: ElevatedButton(
-                  onPressed: () {
-                    _uploadData();
-                  },
-                  child: Text('upload data')),
-            )
+            StreamBuilder(
+                stream: readScores(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final scores = snapshot.data!;
+                    print(scores);
+                    return Text('djfkldf');
+                  }
+                  return Text('slkdfjkldkfjlk');
+                })
           ],
         ),
       ),
